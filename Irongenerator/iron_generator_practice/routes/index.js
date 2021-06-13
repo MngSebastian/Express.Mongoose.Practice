b7bc21e30a125ea026bffba9bfb5835fe434a2a6
MngSebastian <m.sebi97@yahoo.com>
2021-06-13 19:37:58 +0200

    Made a new route that finds and displays every book in the db

--- Irongenerator/iron_generator_practice/routes/index.js
+++ Irongenerator/iron_generator_practice/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const Book = require('../models/book.js');
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then(allTheBooksFromDB => {
+      // console.log('Retrieved books from DB:', allTheBooksFromDB);
+      res.render('books', { books: allTheBooksFromDB})
+    })
+    .catch(error => {
+      console.log('Error while getting the books from the DB: ', error);
+    })
 });
 
 module.exports = router;
