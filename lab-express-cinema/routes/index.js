f7638efdafd12ffd44f288284755816caf945624
MngSebastian <m.sebi97@yahoo.com>
2021-06-13 20:21:49 +0200

    Iteration 3 Done

--- lab-express-cinema/routes/index.js
+++ lab-express-cinema/routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Get Movies Route
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+      .then(allTheMoviesFromDB => {
+        // console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+        res.render('movies', { movies: allTheMoviesFromDB})
+      })
+      .catch(error => {
+        console.log('Error while getting the movies from the DB: ', error);
+      })
+  });
 
 module.exports = router;
