ef7194facff26996c46b1819ea6ea61dfd271c1a
MngSebastian <m.sebi97@yahoo.com>
2021-06-17 11:25:50 +0200

    Iteration 2 Done

--- lab-mongoose-movies/routes/index.js
+++ lab-mongoose-movies/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/Celebrity')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+// Get Celebrities
+router.get('/celebrities', (req, res, next) => {
+  // find all from db
+  Celebrity.find()
+  .then(allCelebritiesFromDb => {
+    res.render('celebrities/index.hbs', { celebrities: allCelebritiesFromDb})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
 module.exports = router;
+
+
