31b03e00f064d243ed4a26d9ac5e6d31fe99f970
MngSebastian <m.sebi97@yahoo.com>
2021-06-21 14:36:01 +0200

    Check if username already in db before creation

--- bcrypt-hello-world/basic-auth/routes/auth.js
+++ bcrypt-hello-world/basic-auth/routes/auth.js
@@ -9,35 +9,52 @@ const bcryptSalt     = 10;
 
 
 router.post("/signup", (req, res, next) => {
-
-
-
-
-  
   
   
   const username = req.body.username;
   const password = req.body.password;
-  const salt     = bcrypt.genSaltSync(bcryptSalt);
-  const hashPass = bcrypt.hashSync(password, salt);
-  
   if (username === "" || password === "") {
     res.render("auth/signup", {
       errorMessage: "Indicate a username and a password to sign up"
     });
     return;
   }
-  User.create({
-    username,
-    password: hashPass
-  })
-  .then(() => {
-    res.redirect("/");
-  })
-  .catch(error => {
-    console.log(error);
-  })
-});
+
+
+
+
+
+
+
+
+
+  
+  User.findOne({ "username": username })
+.then(user => {
+  if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists!"
+      });
+      return;
+    }
+ 
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+ 
+    User.create({
+      username,
+      password: hashPass
+    })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
+    })
+})
+.catch(error => {
+  next(error);
+})})
 
 
 
