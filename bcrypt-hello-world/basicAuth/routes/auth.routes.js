753b46b15952707a4a870d40e4fdff9f568f0724
MngSebastian <m.sebi97@yahoo.com>
2021-06-24 13:40:27 +0200

    Added Form Validation and Password strnght checker

--- bcrypt-hello-world/basicAuth/routes/auth.routes.js
+++ bcrypt-hello-world/basicAuth/routes/auth.routes.js
@@ -1,6 +1,7 @@
 // routes/auth.routes.js
 const { Router } = require('express');
 const router = new Router();
+const mongoose = require('mongoose')
 // setup bcrypt
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
@@ -15,6 +16,22 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
  // .post() route ==> to process form data
 router.post('/signup', (req, res, next) => {
   const { username, email, password } = req.body;
+
+  // Server side validation. This checks if any of the fields are empty - if yes then i 
+  // display errorMsg All fields are mandatory.
+  if (!username || !email || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+
 
   const hashedPassword = bcryptjs.hashSync(password, saltRounds);
  
@@ -36,7 +53,18 @@ router.post('/signup', (req, res, next) => {
       console.log('Newly created user is: ', userFromDB);
       res.redirect('/userProfile');    
     })
-    .catch(error => next(error));
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+    } else if (error.code === 11000) {
+      res.status(500).render('auth/signup', {
+         errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+      });
+    }
+    else {
+        next(error);
+    }
+    });
 });
 
 
