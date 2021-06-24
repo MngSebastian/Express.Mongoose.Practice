608ea8ab9b5a9f4d671cd5760a11dede6bc99a7a
MngSebastian <m.sebi97@yahoo.com>
2021-06-24 12:43:44 +0200

    Signup feature complete

--- lab-express-basic-auth/routes/auth-routes.js
+++ lab-express-basic-auth/routes/auth-routes.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-
 const User = require('../models/User.model.js')
 
-
-// bring bcrypt
-
+// setup bcrypt
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
 
 
 // Signup
@@ -12,5 +11,35 @@ router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
+router.post('/signup', (req, res, next) => {
+    const {username, email, password } = req.body
+
+
+    const hashedPassword = bcryptjs.hashSync(password, saltRounds);
+
+    bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        // username: username
+        username,
+        email,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+      res.redirect('/userProfile');    
+    })
+    .catch(error => next(error));
+});
+
+router.get('/userProfile', (req, res, next) => {
+    res.render('userProfile')
+})
 
 module.exports = router
